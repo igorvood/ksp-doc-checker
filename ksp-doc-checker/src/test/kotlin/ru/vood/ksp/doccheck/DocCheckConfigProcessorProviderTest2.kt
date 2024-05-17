9618a710ecc34922871dcb318e92928a75f6cfa3
vood <igor-vood@gmail.com>
2024-05-17 18:44:42 +0300

    init

--- ksp-doc-checker/src/test/kotlin/ru/vood/ksp/doccheck/DocCheckConfigProcessorProviderTest2.kt
+++ ksp-doc-checker/src/test/kotlin/ru/vood/ksp/doccheck/DocCheckConfigProcessorProviderTest2.kt
@@ -3,7 +3,14 @@ package ru.vood.ksp.doccheck
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.datatest.withData
 class DocCheckConfigProcessorProviderTest2: FunSpec({
-    withData(TestCase(),TestCase(),){
+    withData(
+        TestCase("1", 1),
+        TestCase("2", 2),)
+    {
 
     }
-})
+}){
+    companion object{
+        val class121 = DocCheckConfigProcessorProviderTest2::class.java
+    }
+}
--- ksp-doc-checker/src/test/kotlin/ru/vood/ksp/doccheck/asdsad.kt
+++ ksp-doc-checker/src/test/kotlin/ru/vood/ksp/doccheck/asdsad.kt
@@ -4,7 +4,7 @@ import io.kotest.datatest.WithDataTestName
 
 data class TestCase(
     val description: String ="ASdasd",
-//    val mutation: IMutation,
+    val numTest: Int,
 //    val expected: IExpected,
 ) : WithDataTestName {
     override fun dataTestName(): String = description
